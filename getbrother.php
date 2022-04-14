a5888d65429d11d0d798e9f56a201a0fa9813866
Armando Ortiz <armando@sdcsecurity.com>
2022-04-14 15:44:25 -0700

    This is the initial version of getbrother.php.
    I made it to primarily use it in Zabbix, so copy it over to the location of external scripts for Zabbix, make it executable, and call it as an external check within the templates:
    
    For black toner:
    getbrother.php[{HOST.IP},81]
    
    For cyan toner:
    getbrother.php[{HOST.IP},82]
    
    For magenta toner:
    getbrother.php[{HOST.IP},83]
    
    For yellow toner:
    getbrother.php[{HOST.IP},84]
    
    It can also be used in Nagios or other infrastructure monitoring tools on Linux.  It might also work in Windows, but this wasn't developed there.
    
    You can also call it stand-alone if it's executable:
    
    ./getbrother.php <ip> <code>
    
    Example:
      ./getbrother.php 192.168.100.106 81

---
+++ getbrother.php
@@ -0,0 +1,36 @@
+#!/usr/bin/php
+<?php
+error_reporting(0);
+
+if( !filter_var( $argv[ 1 ], FILTER_VALIDATE_IP ) )
+{
+  die( "Invalid IP address.\n" );
+}
+
+if( strlen( $argv[ 2 ] ) != 2 )
+{
+	die( "Second argument must be a valid hex value.\n" );
+}
+
+// This returns a hex string that we have to parse
+$thisOID = "1.3.6.1.4.1.2435.2.3.9.4.2.1.5.5.8.0";
+
+$thisObject = snmp2_get( $argv[ 1 ], "public", $thisOID );
+
+$thisResult = explode( ":", str_replace( array( " ", "\n" ), "", $thisObject ) );
+
+$myHexVals = explode( "\n", chunk_split( $thisResult[ 1 ], 14 ) );
+
+while( list( $key, $val ) = each( $myHexVals ) )
+{
+	$thisMetric = substr( $val, 0, 2 );
+	$thisMetricVal = substr( $val, 6 );
+
+	if( $thisMetric != "" )
+		$theseVals[ $thisMetric ] =  hexdec( $thisMetricVal );
+}
+
+echo trim( $theseVals[ $argv[ 2 ] ] );
+
+//syslog( LOG_INFO, "IP:{$argv[ 1 ]},CODE:{$argv[ 2 ]},RESULT:{$theseVals[ $argv[ 2 ] ]}" );
+
